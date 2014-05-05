7f6e8cca4d9bd609d048dc0d3cea66b878bb609a
tommybb <tdudzik@gmail.com>
2014-05-05 19:54:47 +0200

    decent_exposure_tender_implementation

--- app/controllers/tenders_controller.rb
+++ app/controllers/tenders_controller.rb
@@ -2,32 +2,19 @@ class TendersController < ApplicationController
   before_action :set_tender, only: [:show, :edit, :update, :destroy]
 
   expose(:tender, attributes: :tender_params)
+  expose(:tenders)
+  expose(:search){ tenders.search(params[:q]) }
+  expose(:filtered_tenders){ search.result.page(params[:page]) }
 
-  # GET /tenders
-  def index
-    @tenders = Tender.order(:created_at).page params[:page]
-  end
 
-  # GET /tenders/1
-  def show
-  end
-
-  # GET /tenders/new
-  def new
-    @tender = Tender.new
-  end
-
-  # GET /tenders/1/edit
-  def edit
-  end
 
   # POST /tenders
   def create
-    @tender = Tender.new(tender_params)
-    @tender.user_id=current_user.id
+    tender = Tender.new(tender_params)
+    tender.user_id=current_user.id
 
-    if @tender.save
-      redirect_to @tender, notice: 'Tender was successfully created.'
+    if tender.save
+      redirect_to tender, notice: 'Tender was successfully created.'
     else
       render :new
     end
@@ -35,8 +22,8 @@ class TendersController < ApplicationController
 
   # PATCH/PUT /tenders/1
   def update
-    if @tender.update(tender_params)
-      redirect_to @tender, notice: 'Tender was successfully updated.'
+    if tender.update(tender_params)
+      redirect_to tender, notice: 'Tender was successfully updated.'
     else
       render :edit
     end
@@ -44,14 +31,14 @@ class TendersController < ApplicationController
 
   # DELETE /tenders/1
   def destroy
-    @tender.destroy
+    tender.destroy
     redirect_to tenders_url, notice: 'Tender was successfully destroyed.'
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_tender
-      @tender = Tender.find(params[:id])
+      tender = Tender.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
